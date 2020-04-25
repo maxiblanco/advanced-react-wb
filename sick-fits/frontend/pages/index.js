a13bcdd08a25781e3f05fcab198f0185ac1e3d6e
Maximiliano Blanco <inkcoming@protonmail.com>
2020-04-25 19:28:35 -0300

    Setup basic nav and routing. Styled comp rendering.

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
 
 const Home = (props) => (
   <div>
     <p>Hey!</p>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
   </div>
 );
 
